824a0fb0e43b12f151ea6ffaea483e05a4dfcd70
Xtal Osc <xtalosc@gmx.com>
2016-09-06 16:11:32 -0500

    first commit

---
+++ jq.js
@@ -0,0 +1,49 @@
+console.log('jq.js sourced');
+
+//global variables
+redCount=0;
+yellowCount=0;
+greenCount=0;
+blueCount=0;
+color='';
+
+$(document).ready(function(){
+  console.log('document ready');
+
+
+  $( 'body' ).on( 'click', '.color-button', function(){
+    // get data-color of clicked button
+    console.log( 'in on click', $( this ).attr( 'data-color' ) );
+
+    color = $( this ).attr( 'data-color');
+
+    //if button clicked, append colored div to container
+    $('.container').append('<div class="'+color+'"><span class="color-cube">' +'</span></div>');
+
+    if (color=='red'){
+      //update color counter
+      redCount++
+      //update <p> tag to show the correct number of clicks
+      $('#red').text('Total red: ' + redCount);
+    }
+    else if (color=='yellow'){
+      //update color counter
+      yellowCount++
+      //update <p> tag to show the correct number of clicks
+      $('#yellow').text('Total yellow: ' + yellowCount);
+    }
+    else if (color=='green'){
+      //update color counter
+      greenCount++
+      //update <p> tag to show the correct number of clicks
+      $('#green').text('Total green: ' + greenCount);
+    }
+    else if (color=='blue'){
+      //update color counter
+      blueCount++
+      //update <p> tag to show the correct number of clicks
+      $('#blue').text('Total blue: ' + blueCount);
+    }
+
+  });//end button click
+});//end document ready
